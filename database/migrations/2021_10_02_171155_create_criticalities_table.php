bc4e982a14681adc79ec32a043181a6452e69759
Lelisvaldo A Gomes <lelisvaldo@outlook.com>
2021-10-02 17:17:57 -0300

    Create Controller, Model, Migration

---
+++ database/migrations/2021_10_02_171155_create_criticalities_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCriticalitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('criticalities', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('criticalities');
+    }
+}
